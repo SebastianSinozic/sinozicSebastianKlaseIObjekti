5edf14f0ee88e223981d019399c12e09b3c5195d
SebastianSinozic <55134663+SebastianSinozic@users.noreply.github.com>
2020-03-10 11:42:53 +0100

    Add files via upload

---
+++ sinozicSebastianVjezbaKiO/sinozicSebastianVjezbaKiO/Neboder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sinozicSebastianVjezbaKiO
+{
+    class Neboder
+    {
+        private int visina, brojKatova;
+
+        public Neboder (int visina, int brojKatova)
+        {
+            this.visina = visina;
+            this.brojKatova = brojKatova;
+        }
+
+        public Neboder()
+        {
+
+        }
+        public int getVisina()
+        {
+            return visina;
+
+        }
+        public void setVisina(int visina)
+        {
+            this.visina = visina;
+        }
+        public int getBrojKatova()
+        {
+            return brojKatova;
+        }
+        public void setBrojKatova()
+        {
+            this.brojKatova = brojKatova;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+
+        }
+    }
+    
+}
---
+++ sinozicSebastianVjezbaKiO/sinozicSebastianVjezbaKiO/Vozilo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sinozicSebastianVjezbaKiO
+{
+    class Vozilo
+    {
+        private char vrsta;
+        private int maxBrzina, brojKotaca;
+
+
+        public Vozilo(char vrsta, int brojKotaca, int maxBrzina)
+        {
+            this.vrsta = vrsta;
+            this.brojKotaca = brojKotaca;
+            this.maxBrzina = maxBrzina;
+        }
+        public Vozilo()
+        {
+
+        }
+        public int getVrsta()
+        {
+            return vrsta;
+
+        }
+
+
+    }
+}
+
+    
+   
